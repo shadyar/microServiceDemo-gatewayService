25811c7fa426097152fb41abb601d3523373e444
Shadyar Khodayari <shadyar81@gmail.com>
2021-08-15 16:45:29 +0430

    Aggregation of Eureka & Gateway config added.

--- gateway-service/src/main/java/com/example/demo/GatewayApplication.java
+++ gateway-service/src/main/java/com/example/demo/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class GatewaytestApplication {
+@EnableEurekaClient
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewaytestApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
